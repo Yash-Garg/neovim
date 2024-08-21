71bbcd8897ec63141accc8e25e59c1bb20ad7e47
Yash-Garg <me@yashgarg.dev>
2024-08-21 21:18:56 +0530

    plugins: enable bufferline

--- modules/nixvim/bufferline/default.nix
+++ modules/nixvim/bufferline/default.nix
@@ -1,11 +1,18 @@
 {
   plugins.bufferline = {
-    enable = false;
+    enable = true;
     settings.options = {
-      hover.enabled = true;
-      indicator.style = null;
+      always_show_bufferline = true;
+      color_icons = true;
+      diagnostics = "nvim_lsp";
+      hover.enabled = false;
+      maxNameLength = 32;
+      maxPrefixLength = 24;
       mode = "buffers";
-      separatorStyle = "slant";
+      persist_buffer_sort = true;
+      separatorStyle = "padded_slope";
+      show_tab_indicators = true;
+      tab_size = 18;
       themable = true;
     };
   };
