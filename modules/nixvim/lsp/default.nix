8341a46ddf8e43a5ec636e3b384aa7f2082099bb
Yash Garg <me@yashgarg.dev>
2026-02-05 00:19:42 +0530

    chore: cleanup unused LSPs

--- modules/nixvim/lsp/default.nix
+++ modules/nixvim/lsp/default.nix
@@ -11,15 +11,11 @@
       dartls.enable = true;
       docker_compose_language_service.enable = true;
       eslint.enable = true;
-      gleam.enable = true;
       golangci_lint_ls.enable = true;
       gopls.enable = true;
-      helm_ls.enable = true;
       html.enable = true;
       jsonls.enable = true;
       kotlin_language_server.enable = true;
-      lua_ls.enable = true;
-      marksman.enable = true;
       nixd.enable = true;
       ruff.enable = true;
       rust_analyzer = {
@@ -40,7 +36,6 @@
         };
       };
       sqls.enable = true;
-      svelte.enable = true;
       tailwindcss.enable = true;
       ts_ls = {
         enable = true;
@@ -77,9 +72,7 @@
           };
         };
       };
-      vue_ls.enable = true;
       yamlls.enable = true;
-      zls.enable = true;
     };
   };
 }
